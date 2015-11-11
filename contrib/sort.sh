c0752a608af23c21e8a626be5629de36ba15d5c6
Koen Kooi <koen@dominion.thruhere.net>
2015-11-11 14:16:11 +0100

    contrib: sort.sh: sync with feed server
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- contrib/sort.sh
+++ contrib/sort.sh
@@ -5,7 +5,7 @@
 
 export PATH=$PATH:~/bin/
 
-ipkg_tools_path="/home/koen/bin"
+ipkg_tools_path="/usr/bin"
 
 if [ $(basename $PWD) != "unsorted" ] ; then
 	echo "Not in feed dir! Exiting"
@@ -59,7 +59,7 @@ case "$arch" in
 	"486sx")
 			machines="vortex86sx" ;;
 	"aarch64")
-			machines="genericarmv8" ;;
+			machines="genericarmv8 hikey dragonboard-410c" ;;
 	"armv4")
 			machines="collie h3600 h3800 htcwallaby jornada56x jornada7xx shark simpad" ;;
 	"armv4t")
@@ -83,6 +83,9 @@ case "$arch" in
  palmz71 palmz72 poodle qemuarm rokre2 rokr-e2 rokre6 ronetix-pm9261
  ronetix-pm9263 sgh-i900 sheevaplug spitz stamp9g20evb topas910 tosa
  triton ts409 tsx09 tx25 tx27" ;;
+	"armv5e")
+			archdir="armv5te" 
+			machines="" ;;
 	"armv5te-mx23")
 			archdir="armv5te" 
 			machines="" ;;
@@ -93,16 +96,22 @@ case "$arch" in
 			machines="fsg3be ixp4xxbe" ;;
 	"xscaleteb")
 			machines="nslu2be";;
+	"xscaleeb")
+			archdir="xscaleteb" 
+			machines="" ;;
 	"armv6")
 			machines="bug iphone mx31ads mx31moboard mini6410 nokia800 omap2420h4 omap2430sdp pcm043 smartq5 smartqv7 smdk6410" ;;
 	"armv6-vfp")
 			machines="raspberrypi" ;;
+	"arm1176jzfshf-vfp")
+			archdir="armv6-vfp" 
+			machines="" ;;
 	"armv6-novfp")
 			machines="htcblackstone htcdiamond htcdream htckaiser htcnike htcpolaris htcraphael htctitan htcvogue" ;;
 	"armv7at2hf-vfp")
-			machines="sama5d3_xplained sama5d3xek" ;;
+			machines="sama5d3_xplained sama5d3xek colibri-vf" ;;
 	"armv7at2hf-vfp-neon")
-			machines="am335x-evm am3517-evm am37x-evm arndale arndale-octa beagleboard beaglebone chromebook-snow cubieboard cubieboard2 cubietruck dra7xx-evm efikamx genericarmv7a hpveer htcleo imx6qsabrelite keystone-evm nexusone nokia900 olinuxino-a10s olinuxino-a13 olinuxino-a20 omap3evm omap5-evm om-gta04 overo pandaboard wandboard-solo wandboard-dual wandboard-quad" ;;
+			machines="am335x-evm am3517-evm am37x-evm arndale arndale-octa bananapi beagleboard beaglebone chromebook-snow cubieboard cubieboard2 cubietruck dra7xx-evm efikamx forfun-q88db genericarmv7a hpveer htcleo ifc6410 imx6qsabrelite keystone-evm mele meleg nexusone nokia900 olinuxino-a10 olinuxino-a10s olinuxino-a13 olinuxino-a20 olinuxino-a20lime olinuxino-a20som  omap3evm omap5-evm om-gta04 overo pandaboard raspberrypi2 wandboard-solo wandboard-dual wandboard-quad" ;;
 	"armv7at2hf-vfp-neon-mx5")
 			archdir="armv7ahf-vfp-neon"
 			machines="" ;;
@@ -118,6 +127,12 @@ case "$arch" in
 	"bfin")
 			archdir="blackfin"
 			machines="adsp-bf537-stamp" ;;
+	"cortexa5t2hf-vfp-neon")
+			archdir="armv7ahf-vfp-neon"
+			machines="" ;;
+	"cortexa5hf-vfp-neon")
+			archdir="armv7ahf-vfp-neon"
+			machines="" ;;
 	"cortexa9hf-vfp-neon")
 			machines="" ;;
 	"cortexa9hf-vfp-neon-mx6")
@@ -132,6 +147,9 @@ case "$arch" in
 	"i686")
 			machines="eee701 i686-generic guinness progear ion vmware x86-32-nocona x86-prescott" ;;
 	"iwmmxt")
+			machines="" ;;
+	"mips32r2")
+			archdir="mips32"
 			machines="" ;;
 	"mips32")
 			machines="qemumips ben-nanonote" ;;
@@ -170,7 +188,10 @@ case "$arch" in
 	"core2-64")
 			machines="qemux86-64 dominion" ;;
 	"core2-32")
-			machines="crownbay-noemgd emenlow-noemgd fri2-noemgd" ;;
+			machines="crownbay-noemgd emenlow-noemgd fri2-noemgd edison" ;;
+	"core2-32-intel-common")
+			archdir="core2-32"
+			machines="" ;;
 	"core2-32-emgd")
 			archdir="core2-32"
 			machines="crownbay emenlow fri2 minnow n450 soekris-net6501" ;;
@@ -270,7 +291,7 @@ for i in `find . -name  "*.ipk"| grep _all` ; do mkdir -p ../all/ || true ;mv $i
 mkdir -p ../sdk ; mv *sdk.ipk ../sdk/ || true
  (mkdir -p ../sdk ; cd ../sdk && ${ipkg_tools_path}/opkg-make-index -p Packages -m . >& /dev/null ; touch Packages.sig )
 
-for arch in 486sx aarch64 armv4t armv4 armv5teb xscaleteb armv5te-mx23 armv5te-mx28 armv5te armv6-vfp armv6-novfp armv6 armv7a-vfp armv7a-vfp-neon armv7ahf-vfp-neon armv7ahf-vfp-neon-mx5 armv7ahf-vfp-neon-mx6 armv7ahf-vfp armv7at2hf-vfp armv7at2hf-vfp-neon armv7at2hf-vfp-mx5 armv7at2hf-vfp-mx6 avr32 bfin cortexa8hf-vfp-neon cortexa9hf-vfp-neon-mx6 cortexa9hf-vfp-neon geode i486 i586 i686 iwmmxt mips32 mipsel powerpc ppc405 ppc440e ppc603e ppce300c2 ppce300c3 ppce500v2 ppce500 ppce600 sh4 sparc corei7-64-intel-common corei7-64 x86 corei7-64 core2-64 core2-32 core2-32-emgd core2; do
+for arch in 486sx aarch64 armv4t armv4 armv5e armv5teb xscaleteb xscaleeb armv5te-mx23 armv5te-mx28 armv5te armv6-vfp armv6-novfp arm1176jzfshf-vfp armv6 armv7a-vfp armv7a-vfp-neon armv7ahf-vfp-neon armv7ahf-vfp-neon-mx5 armv7ahf-vfp-neon-mx6 armv7ahf-vfp armv7at2hf-vfp armv7at2hf-vfp-neon armv7at2hf-vfp-mx5 armv7at2hf-vfp-mx6 avr32 bfin cortexa5t2hf-vfp-neon cortexa5hf-vfp-neon cortexa8hf-vfp-neon cortexa9hf-vfp-neon-mx6 cortexa9hf-vfp-neon geode i486 i586 i686 iwmmxt mips32r2 mips32 mipsel powerpc ppc405 ppc440e ppc603e ppce300c2 ppce300c3 ppce500v2 ppce500 ppce600 sh4 sparc corei7-64-intel-common corei7-64 x86 corei7-64 core2-64 core2-32-intel-common core2-32 core2-32-emgd core2; do
 
 	do_sort
 done
