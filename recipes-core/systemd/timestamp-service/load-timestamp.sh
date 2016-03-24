f72cfba00fea6a715491ba47ab89c9a873c97ec7
Max Krummenacher <max.oss.09@gmail.com>
2016-03-24 20:04:19 +0100

    timestamp-service: use new format in /etc/timestamp
    The following commit changed the format of /etc/timestamp to include
    additionally seconds when initially created. e.g. the file now contains
    20160223174913
    
    https://github.com/Angstrom-distribution/meta-angstrom/commit/8228b2d56ef2415c8b96d1463fb38816279d4c74
    
    Follow that change for the systemd implementation of the poor man's RTC.
    
    Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- recipes-core/systemd/timestamp-service/load-timestamp.sh
+++ recipes-core/systemd/timestamp-service/load-timestamp.sh
@@ -3,19 +3,19 @@
 if [ "$1" != "--save" ] ; then
 
 # Set the system clock from timestamp file
-# if the timestamp is 1 minute or more recent
+# if the timestamp is 1 second or more recent
 # than the current systemtime.
 
-SYSTEMDATE=$(/bin/date -u "+%4Y%2m%2d%2H%2M")
+SYSTEMDATE=$(/bin/date -u "+%4Y%2m%2d%2H%2M%2S")
 
 TIMESTAMP=$(/bin/cat /etc/timestamp 2>/dev/null)
 
 if [ $SYSTEMDATE -lt $TIMESTAMP ] 2>/dev/null ; then
 	echo "Update systemtime from /etc/timestamp"
-	/bin/date -u ${TIMESTAMP#????}${TIMESTAMP%????????}
+	/bin/date -u ${TIMESTAMP:4:8}${TIMESTAMP:0:4}.${TIMESTAMP:(-2)}
 fi
 
 else
 # Store the current systemtime in /etc/timestamp
-	/bin/date -u +%4Y%2m%2d%2H%2M > /etc/timestamp
+	/bin/date -u +%4Y%2m%2d%2H%2M%2S > /etc/timestamp
 fi
