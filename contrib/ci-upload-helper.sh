57a4fff0525e9a52b9a3d33b3bbf82ca6b06431c
Koen Kooi <koen@dominion.thruhere.net>
2019-03-12 20:25:06 +0100

    contrib: ci-upload-helper.sh: add debugging output
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- contrib/ci-upload-helper.sh
+++ contrib/ci-upload-helper.sh
@@ -6,7 +6,11 @@ nightlydir="$2"
 # create directory on remote machines
 ssh ${HOST} "mkdir -p ${nightlydir}"
 
+ls -la
+
 mkdir -p deploy/glibc/images
+
+find deploy/glibc/images
 
 # Compress raw images and extX files if needed
 for i in $(find deploy/glibc/images/ -name "A*.ext3"; find deploy/glibc/images/ -name "A*img" ; find deploy/glibc/images/ -name "A*.ext2" ; find deploy/glibc/images/ -name "A*.ext4" ; find deploy/glibc/images/ -name "A*sdcard" ; find deploy/glibc/images/ -name "A*iso") ; do xz -f -v -z -T0 -9 -e $i ; done
